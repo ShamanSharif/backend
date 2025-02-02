03a6f1f7d7274087c7461a3aa0572ba9d03bc97e
Niloy Mallick <mallickniloy@gmail.com>
2025-02-02 23:07:28 +0600

    csrf

--- cellNstyle_website/settings.py
+++ cellNstyle_website/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
-CSRF_TRUSTED_ORIGINS = ['https://cellnstyle.com']
+CSRF_TRUSTED_ORIGINS = ['https://cellnstyle.com', 'https://www.cellnstyle.com']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
